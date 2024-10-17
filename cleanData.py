c8ee00ce5c9fd73f039780e9e53008627702cc9b
Ben Chartoff <ben.chartoff@excella.com>
2024-10-16 20:09:15 -0400

    clean looker data w gpt input

--- cleanData.py
+++ cleanData.py
@@ -1,6 +1,7 @@
 # location, month, month num, day, day of week, hour, minute
 import csv
 import re
+import math
 from datetime import datetime, timezone
 from pytz import timezone, utc
 from timezonefinder import TimezoneFinder
@@ -10,150 +11,17 @@ tzf = TimezoneFinder()
 
 
 def convertByTimezone(dt, lat, lng):
-
     tz = timezone(tzf.certain_timezone_at(lng=float(lng), lat=float(lat)))
     return dt.astimezone(tz)
 
 
 STATES = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Minor Outlying Islands", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "U.S. Virgin Islands", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-f = []
-def guessLocationType(loc):
-	locComponents = loc.split(",")
-	# if(str.find(loc, ""))
-	matchPatterns = [
-		["restaurant", "Restaurant"],
-		["Kitchen", "Restaurant"],
-		["eatery", "Restaurant"],
-		["Coffee", "Restaurant"],
-		["bakery", "Restaurant"],
-		["burger", "Restaurant"],
-		["Winery", "Restaurant"],
-		["vineyards", "Restaurant"],
-		["pizza", "Restaurant"],
-		["avenue", "Neighborhood"],
-		["street", "Neighborhood"],
-		["times square", "Neighborhood"],
-		["Santa Monica Pier", "Neighborhood"],
-		["horseshoe bend", "Natural Attraction"],
-		["Hollywood Walk of Fame", "Place of Interest"],
-		["Empire State Building", "Place of Interest"],
-		["Antelope Canyon", "Natural Attraction"],
-		["Bellagio Las Vegas", "Travel"],
-		["Las Vegas Strip", "Neighborhood"],
-		["Griffith Observatory", "Place of Interest"],
-		["Lincoln Memorial", "Place of Interest"],
-		["Monument Valley", "Natural Attraction"],
-		["United States Capitol", "Place of Interest"],
-		["PIER 39", "Neighborhood"],
-		["Hollywood Sign", "Place of Interest"],
-		["Lower Antelope Canyon", "Natural Attraction"],
-		["Statue of Liberty National Monument", "Place of Interest"],
-		["Venice Beach", "Neighborhood"],
-		["DUMBO", "Neighborhood"],
-		["The High Line", "Neighborhood"],
-		["Hollywood", "Neighborhood"],
-		["Rockefeller Center", "Place of Interest"],
-		["Grand Central Terminal", "Travel"],
-		["The Venetian Resort Las Vegas", "Travel"],
-		["Union Square, San Francisco", "Neighborhood"],
-		["Fisherman's Wharf", "Neighborhood"],
-		["The White House", "Place of Interest"],
-		["Millennium Park", "Place of Interest"],
-		["grand canyon", "Natural Attraction"],
-		["museum", "Place of Interest"],
-		["national park", "Natural Attraction"],
-		["state park", "Natural Attraction"],
-		["bridge", "Natural Attraction"],
-		["central park", "Natural Attraction"],
-		["niagara falls", "Natural Attraction"],
-		["botanical garden", "Natural Attraction"],
-		["botanic garden", "Natural Attraction"],
-		["universal studios", "Place of Interest"],
-		["disney", "Place of Interest"],
-		["busch gardens", "Place of Interest"],
-		["Wizarding World", "Place of Interest"],
-		["zoo","Place of Interest"],
-		["airport", "Travel"],
-		["LAX", "Travel"],
-		["statue of liberty", "Place of Interest"],
-		["World Trade Center", "Place of Interest"],
-		["hoover dam", "Place of Interest"],
-		["Beach", "Natural Attraction"],
-		["soho", "Neighborhood"],
-		["Lake Powell", "Natural Attraction"],
-		["Death Valley", "Natural Attraction"],
-		["bubba gump", "Restaurant"],
-		["downtown", "Neighborhood"],
-		["district", "Neighborhood"],
-		["chinatown", "Neighborhood"],
-		["denny's", "Neighborhood"],
-		["the signature room","Restaurant"],
-		["Upper East Side","Neighborhood"],
-		["Stearns Wharf","Neighborhood"],
-		["hotel","Travel"],
-		["eataly","Restaurant"],
-		["lake","Natural Attraction"],
-		["Eagle Point","Natural Attraction"],
-		["Chicago River","Neighborhood"],
-		["Yard House","Neighborhood"],
-		["apartments","Neighborhood"],
-		["beacon hill","Neighborhood"],
-		["roosevelt island","Natural Attraction"],
-		["national preserve","Natural Attraction"],
-		["San Francisco Bay Area","Neighborhood"],
-		["midtown","Neighborhood"],
-		["waterfront","Neighborhood"],
-		["river","Natural Attraction"],
-		["falls","Natural Attraction"],
-		["New York City","City"],
-		["chelsea","Neighborhood"],
-		["St. Louis","City"],
-		["ihop","Restaurant"],
-		["Washington, DC","City"],
-		["Williamsburg, Brooklyn","Neighborhood"],
-		["Capitol Hill","Neighborhood"],
-		["Boulevard","Neighborhood"],
-		["Badwater Basin","Natural Attraction"],
-		["New York City - Manhattan - Nyc","City"],
-		["Bryant Park","Natural Attraction"],
-		["Navy Pier","Neighborhood"],
-		["white sands","Natural Attraction"],
-		["Little Havana","Neighborhood"],
-		["Kerry Park","Neighborhood"],
-		["La Jolla Cove","Natural Attraction"],
-		["Balboa Park","Neighborhood"],
-		["Washington Square Park","Natural Attraction"],
-		["Georgetown","Neighborhood"],
-		["union station","Travel"],
-		["cheesecake","Restaurant"],
-		["Boston Harbor","Neighborhood"],
-		["Golden Gate Park","Natural Attraction"],
-		["Madison Square Park","Neighborhood"],
-		["Battery Park","Neighborhood"],
-		["Glacier Point","Natural Attraction"],
-		["yosemite","Natural Attraction"]
-	]
-
-	if locComponents[-1].strip() in STATES and len(locComponents) == 2:
-		return "City"
-
-	if loc.strip() in STATES:
-		return "State"
-
-	for mp in matchPatterns:
-		if re.search(mp[0], loc, re.IGNORECASE):
-			return mp[1]
-
-	return ""
-
 def reformatLocationFile():
-	tmpLocs = []
 	with open('data/Location.csv', newline='') as csvfile:
 		cr = csv.reader(csvfile, delimiter=',', quotechar='"')
 		for row in cr:
 			loc = row[0]
-			locCount = locData[loc]["locCount"]
 			geoLoc = row[4]
 			lat = row[5]
 			lon = row[6]
@@ -179,19 +47,10 @@ def reformatLocationFile():
 				# e.g. "Cape Cod Bay, Plymouth, United States of America"
 				continue
 
-			locType = guessLocationType(loc)
-			if(loc not in tmpLocs):
-				if(locType == "" and locCount >= 10):
-					# default location type, checked as valid for all location Counts >= 10
-					# small location counts are not part of analysis data set
-					locType = "Place of Interest"
-				if locType != "":
-					locData[loc]["locType"] = locType
-					locData[loc]["state"] = state
-					locData[loc]["lat"] = lat
-					locData[loc]["lon"] = lon
 
-					tmpLocs.append(loc)
+			locData[loc]["state"] = state
+			locData[loc]["lat"] = lat
+			locData[loc]["lon"] = lon
 
 temperatureData = {}
 def getTemperatureData():
@@ -202,25 +61,16 @@ def getTemperatureData():
 			state = row[0]
 			month = str(int(row[4]))
 			year = row[5]
-			temp = row[2]
+			# round down to nearest 10
+			temp = math.floor(float(row[2])/10)*10
 			temperatureData[state + "_" + year + "-" + month ] = temp
-
-
 
 def buildBigTable():
 	with open('data/IG.csv', newline='') as csvfile:
 		cr = csv.reader(csvfile, delimiter=',', quotechar='"')
 		h = next(cr)
 		cw = csv.writer(open("data/lookerData.csv","w"))
-		cw.writerow(["location","locationType","state","lat","lon","month","year","dayOfWeek","hour","minute","temperature"])
-
-					# locData[loc]["locType"] = locType
-					# locData[loc]["state"] = state
-					# locData[loc]["lat"] = lat
-					# locData[loc]["lon"] = lon
-
-					# temperatureData[state + "_" + year + "-" + month ] = temp
-
+		cw.writerow(["location","category","parentCategory","state","lat","lon","dateString","month","year","dayOfWeek","dayOfWeekNum","hour","minute","temperature"])
 
 		for row in cr:
 
@@ -236,13 +86,11 @@ def buildBigTable():
 
 			location = locRaw
 			locationType = locObj["locType"]
+			locationParentType = locObj["locParentType"]
 			state = locObj["state"]
 			lat = locObj["lat"]
 			lon = locObj["lon"]
 			dateObj = convertByTimezone(dateObjRaw, lat, lon)
-			# dateObj = dateObjRaw
-
-			# print(dateObj)
 
 			month = dateObj.month
 			year = dateObj.year
@@ -257,11 +105,7 @@ def buildBigTable():
 
 			temperature = temperatureData[temperatureKey]
 
-			cw.writerow([location,locationType,state,lat,lon,month,year,dayOfWeek,dayOfWeekNum,hour,minute,temperature])
-
-
-
-
+			cw.writerow([location,locationType,locationParentType,state,lat,lon,datetimeRaw,month,year,dayOfWeek,dayOfWeekNum,hour,minute,temperature])
 
 
 locData = {}
@@ -275,6 +119,15 @@ def createSingleTable():
 				locData[loc] = {"locCount": 1}
 			else:
 				locData[loc]["locCount"] += 1
+	with open('gptLocationClassifications.csv', newline = '') as csvfile:
+		cr = csv.reader(csvfile, delimiter=',', quotechar='"')
+		h = next(cr)
+		for row in cr:
+			if row[0] not in locData:
+				continue
+			locData[row[0]]["locType"] = row[1]
+			locData[row[0]]["locParentType"] = row[2]
+
 
 	reformatLocationFile()
 	getTemperatureData()
